07cad5265e0b962b23880ff5face007ef14c18a2
dotsenkoart-code <dotsenkoart@gmail.com>
2026-03-04 21:45:07 -0800

    sw_cmd.js

--- sw_cmd.js
+++ sw_cmd.js
@@ -1,16 +1,32 @@
-const CACHE_NAME = 'cmd-cache-v1';
+const CACHE_NAME = 'cmd-cache-v2';
 const urlsToCache = [
   './index.html',
   './manifest_cmd.json',
-  './icon-cmd.jpg'
+  './icon-cmd192.png',
+  './icon-cmd512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
